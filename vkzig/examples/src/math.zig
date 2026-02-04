097685c85b1bcee4494cc84e5231dcfa7925ef11
leszek <rhoninn11@gmail.com>
2026-02-04 17:42:00 +0100

    - testing testing

--- vkzig/examples/src/math.zig
+++ vkzig/examples/src/math.zig
@@ -3,6 +3,7 @@ const std = @import("std");
 const vec3 = @Vector(3, f32);
 const vec4 = @Vector(4, f32);
 
+// column-major
 const mat4 = [4]vec4;
 
 const hmm_a: vec3 = .{ 1, 1, 1 };
@@ -48,8 +49,7 @@ pub fn trim3d(a: vec4) vec3 {
 }
 
 pub fn dot(a: vec3, b: vec3) f32 {
-    const squared = a[X] * b[X] + a[Y] * b[Y] + a[Z] * b[Z];
-    return squared;
+    return @reduce(.Add, a * b);
 }
 
 pub fn cross(a: vec3, b: vec3) vec3 {
@@ -60,11 +60,20 @@ pub fn cross(a: vec3, b: vec3) vec3 {
     };
 }
 
+pub fn len(vec: vec3) f32 {
+    return std.math.sqrt(dot(vec, vec));
+}
+
+test "len test" {
+    const v = vec3{ 1, 0, 0 };
+    const neer_zero = len(v) - 1;
+    try std.testing.expect(abs(neer_zero) < 0.001);
+}
+
 test "if it even crossing" {
     const y = vec3{ 0, 1, 0 };
     const z = vec3{ 0, 0, 1 };
-    const x = cross(z, y);
-    // std.debug.print("!!! cross result: {d} {d} {d}\n", .{ x[0], x[1], x[2] });
+    const x = cross(z, y); // right handed
     try std.testing.expect(x[0] < -0.9999);
     try std.testing.expect(x[1] < 0.0001);
     try std.testing.expect(x[2] < 0.0001);
@@ -80,7 +89,7 @@ const mat4u = extern union {
     arr: [16]f32,
 };
 
-pub fn matMul(m: mat4, v: vec4) vec4 {
+pub fn matXvec(m: mat4, v: vec4) vec4 {
     var out: vec4 = m[0] * splat4d(v[0]);
     for (1..4) |i| out += m[i] * splat4d(v[i]);
     return out;
@@ -88,22 +97,9 @@ pub fn matMul(m: mat4, v: vec4) vec4 {
 
 test "mat mul test" {
     const v = vec3{ 8, -2, 1 };
-    const b = matMul(mat_identity().mat, stack4d(v, 1));
+    const b = matXvec(mat_identity().mat, stack4d(v, 1));
 
     try std.testing.expect(len(v - trim3d(b)) < 0.001);
-    try std.testing.expect(false);
-    // TODO: not sure if works properly
-}
-
-pub fn len(vec: vec3) f32 {
-    const squared = @reduce(.Add, vec * vec);
-    return std.math.sqrt(squared);
-}
-
-test "len test" {
-    const v = vec3{ 1, 0, 0 };
-    const neer_zero = len(v) - 1;
-    try std.testing.expect(abs(neer_zero) < 0.001);
 }
 
 pub fn norm(v: vec3) vec3 {
@@ -123,12 +119,15 @@ pub fn mat_identity() mat4u {
 }
 
 pub fn mat_ortho(right: f32, left: f32, up: f32, down: f32, far: f32, near: f32) mat4u {
+    const w = right - left;
+    const h = down - up; // Vk has -Y axis
+    const d = far - near;
     const m4: mat4u = .{
-        .arr = .{
-            2 / (right - left), 0,               0,                -(right + left) / (right - left), //
-            0,                  2 / (down - up), 0,                -(down + up) / (down - up),
-            0,                  0,               1 / (far - near), -near / (far - near),
-            0,                  0,               0,                1,
+        .mat = .{
+            .{ 2 / w, 0, 0, 0 }, // column-major
+            .{ 0, 2 / h, 0, 0 },
+            .{ 0, 0, 1 / d, 0 },
+            .{ -(right + left) / w, -(down + up) / h, -near / d, 1 },
         },
     };
     return m4;
@@ -144,10 +143,10 @@ test "ortho to vulkan" {
     const initial = vec3{ 10, 10, -1 };
     const final = vec3{ 1, -1, 0 };
     const mat = mat_ortho(10, 5, 10, 5, 1, -1);
-    const after_transform = matMul(mat.mat, stack4d(initial, 1));
+    const after_transform = matXvec(mat.mat, stack4d(initial, 1));
 
-    mat_print(mat, "ortho");
-    std.debug.print("{}\n", .{after_transform});
+    // mat_print(mat, "ortho");
+    // std.debug.print("{}\n", .{after_transform});
     try std.testing.expect(len(final - trim3d(after_transform)) < 0.001);
 }
 
@@ -175,7 +174,7 @@ pub fn abs(a: f32) f32 {
 
 const minus_vec3 = @as(vec3, @splat(-1));
 
-pub fn mat_look_at(pos: vec3, target: vec3, up: vec3) ![16]f32 {
+pub fn mat_look_at(pos: vec3, target: vec3, up: vec3) !mat4u {
     const delta = target - pos;
     const m_forward = norm(delta);
     if (abs(dot(m_forward, up)) > 0.95) {
@@ -185,13 +184,26 @@ pub fn mat_look_at(pos: vec3, target: vec3, up: vec3) ![16]f32 {
     const m_left = norm(cross(m_forward, up));
     const m_up = cross(m_left, m_forward);
 
-    const mat: mat4 = .{
-        .{ m_left[X], m_up[X], m_forward[X], pos[X] },
-        .{ m_left[Y], m_up[Y], m_forward[Y], pos[Y] },
-        .{ m_left[Z], m_up[Z], m_forward[Z], pos[Z] },
-        stack4d(zero3(), 1),
+    const mat: mat4u = .{
+        .mat = .{
+            stack4d(m_left, 0), //column-major
+            stack4d(m_up, 0),
+            stack4d(m_forward, 0),
+            stack4d(pos, 1),
+        },
     };
-    const dual: mat4u = .{ .mat = mat };
-    return dual.arr;
+    return mat;
     // return mat_identity();
+}
+
+test "is matrix looking" {
+    const random_point = vec3{ 1, 0, 0 };
+
+    const mat = try mat_look_at(.{ 2, 0, 0 }, random_point, .{ 0, 1, 0 });
+
+    const t_point = matXvec(mat.mat, stack4d(random_point, 1));
+    mat_print(mat, "look at");
+    std.debug.print("{}\n", .{t_point});
+    try std.testing.expect(abs(t_point[0]) < 0.001);
+    try std.testing.expect(abs(t_point[1]) < 0.001);
 }
