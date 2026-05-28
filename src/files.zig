c0b1b6685727df054b15974a2944c0e1b37711b5
leszek <rhoninn11@gmail.com>
2026-05-28 20:17:44 +0200

    - zip serch moved to files

--- src/files.zig
+++ src/files.zig
@@ -23,3 +23,150 @@ pub fn stdoutWriter(io: std.Io, buffer: []u8) *std.Io.Writer {
     var writer = stderr.writer(io, buffer);
     return &writer.interface;
 }
+
+const PathList = std.ArrayList([]const u8);
+const PRA_PATH_N: comptime_int = 32;
+pub fn findExt(io: std.Io, arena: std.mem.Allocator, here: []const u8, ext: []const u8) !PathList {
+    const serdes_dir = try std.Io.Dir.cwd().openDir( //
+        io, here, .{ .iterate = true });
+    defer serdes_dir.close(io);
+
+    var iterator = serdes_dir.iterate();
+
+    var found_files: std.ArrayList([]const u8) = try .initCapacity(arena, PRA_PATH_N);
+    while (try iterator.next(io)) |entry| {
+        if (std.mem.endsWith(u8, entry.name, ext)) {
+            const full_path = try std.fs.path.join(arena, &.{ here, entry.name });
+            try found_files.append(arena, full_path);
+        }
+    }
+
+    if (found_files.items.len == 0) return error.NoFile;
+
+    return found_files;
+}
+pub const ZippedFiles = struct {
+    const SliceAlign = @alignOf([]const u8);
+    file_sets: [][][]u8,
+    file_paths: [][]u8,
+    chars: []u8,
+
+    pub fn init(comptime N: usize, gpa: std.mem.Allocator, set_num: usize, glyph_num: usize) !ZippedFiles {
+        return .{
+            .file_sets = try gpa.alloc([][]u8, set_num),
+            .file_paths = try gpa.alloc([]u8, set_num * N),
+            .chars = try gpa.alloc(u8, glyph_num),
+        };
+    }
+
+    pub fn deinit(self: *ZippedFiles, gpa: std.mem.Allocator) void {
+        gpa.free(self.file_sets);
+        gpa.free(self.file_paths);
+        gpa.free(self.chars);
+    }
+};
+pub fn zipSearch(
+    io: std.Io,
+    gpa: std.mem.Allocator,
+    seach_loc: []const u8,
+    comptime exts_zip: []const []const u8,
+) !void {
+    const N = exts_zip.len;
+    const Bins = [N]?u32;
+    const PathArrayList = std.ArrayList([]const u8);
+    const FileMap = std.StringHashMap(Bins);
+
+    const LambdaL = struct {
+        pub fn comptimeMsg() void {
+            std.debug.print("+++ CMPTM | N is: {d} & Bins size is: {d}\n", .{
+                N, @sizeOf(Bins),
+            });
+        }
+        pub fn trimSufix(str: []const u8, sufix: []const u8) []const u8 {
+            const ends = std.mem.endsWith(u8, str, sufix);
+            return if (ends) str[0..(str.len - sufix.len)] else str;
+        }
+        pub fn trimPrefix(str: []const u8, prefix: []const u8) []const u8 {
+            const starts = std.mem.startsWith(u8, str, prefix);
+            return if (starts) str[prefix.len..] else str;
+        }
+
+        pub fn strcompR(_: void, lhs: []const u8, rhs: []const u8) bool {
+            return std.mem.order(u8, lhs, rhs) == .lt;
+        }
+        pub fn strcompL(_: void, lhs: []const u8, rhs: []const u8) bool {
+            return std.mem.order(u8, lhs, rhs) == .gt;
+        }
+        pub fn catchNames(opa: std.mem.Allocator, map: *FileMap) !PathList {
+            var names_of_sort: PathArrayList = try .initCapacity(opa, PRA_PATH_N);
+            defer std.mem.sort([]const u8, names_of_sort.items, {}, strcompR);
+
+            var it = map.iterator();
+            while (it.next()) |e| {
+                var valid = true;
+                for (e.value_ptr) |presence|
+                    valid = (valid and presence != null);
+
+                if (valid)
+                    try names_of_sort.append(opa, e.key_ptr.*);
+            }
+            return names_of_sort;
+        }
+    };
+
+    var arana: std.heap.ArenaAllocator = .init(gpa);
+    defer arana.deinit();
+
+    const aa = arana.allocator();
+    var zip_lists: [N]PathArrayList = undefined;
+
+    for (0..N) |i| {
+        zip_lists[i] = findExt(io, aa, seach_loc, exts_zip[i]) catch {
+            return error.NoZipping;
+        };
+    }
+
+    const EMPTY_BINS: Bins = .{null} ** N;
+    var found_map: FileMap = .init(aa);
+    try found_map.ensureTotalCapacity(32);
+
+    for (0.., zip_lists) |ext_idx, list| {
+        const ext = exts_zip[ext_idx];
+
+        for (0.., list.items) |list_idx, element| {
+            const almost_key = LambdaL.trimPrefix(element, seach_loc);
+            const key = LambdaL.trimSufix(almost_key, ext);
+
+            if (found_map.getPtr(key) == null)
+                try found_map.put(key, EMPTY_BINS);
+
+            if (found_map.getPtr(key)) |val|
+                val[ext_idx] = @as(u32, @intCast(list_idx));
+        }
+    }
+
+    const names_of_sort = try LambdaL.catchNames(aa, &found_map);
+
+    const names_count = names_of_sort.items.len;
+    var glyph_count: usize = 0;
+    for (names_of_sort.items) |name| {
+        for (found_map.get(name).?, 0..) |inices, group_idx| {
+            const path_len = zip_lists[group_idx].items[inices.?].len;
+            glyph_count += path_len;
+        }
+    }
+
+    var zipout: ZippedFiles = try .init(N, gpa, names_count, glyph_count);
+    defer zipout.deinit(gpa);
+
+    var char_offset: usize = 0;
+    for (names_of_sort.items) |name| {
+        const ok = found_map.get(name).?;
+        for (0.., ok) |ext_idx, str_idx| {
+            const path = zip_lists[ext_idx].items[str_idx.?];
+            const sub = zipout.chars[char_offset..][0..path.len];
+            @memcpy(sub, path);
+            char_offset += path.len;
+        }
+    }
+}
--- src/proto.zig
+++ src/proto.zig
@@ -9,6 +9,7 @@ const shu = @import("shaders/utils.zig");
 
 const m = @import("math.zig");
 const input = @import("input.zig");
+const files = @import("files.zig");
 
 const Errorset = error{
     constrained,
@@ -104,131 +105,20 @@ pub fn findSedes(io: std.Io, here: []const u8) ![]const u8 {
     return error.NoSerdes;
 }
 
-const PathList = std.ArrayList([]u8);
-const defPathListSize = 32;
-pub fn findExt(io: std.Io, arena: std.mem.Allocator, here: []const u8, ext: []const u8) !PathList {
-    const serdes_dir = try std.Io.Dir.cwd().openDir( //
-        io, here, .{ .iterate = true });
-    defer serdes_dir.close(io);
-
-    var iterator = serdes_dir.iterate();
-
-    var found_files: std.ArrayList([]u8) = try .initCapacity(arena, defPathListSize);
-    while (try iterator.next(io)) |entry| {
-        if (std.mem.endsWith(u8, entry.name, ext)) {
-            const full_path = try std.fs.path.join(arena, &.{ here, entry.name });
-            try found_files.append(arena, full_path);
-        }
-    }
-
-    if (found_files.items.len == 0) return error.NoFile;
-
-    return found_files;
-}
-fn stringCompGrow(_: void, lhs: []const u8, rhs: []const u8) bool {
-    return std.mem.order(u8, lhs, rhs) == .lt;
-}
-
-pub const SearchResult = struct {
-    const SliceAlign = @alignOf([]const u8);
-    file_packs: []const []const []const u8,
-    file_paths: []const []const u8,
-    chars: []const u8,
-
-    pub fn deinit(self: SearchResult, gpa: std.mem.Allocator) void {
-        gpa.free(self.file_packs);
-        gpa.free(self.file_paths);
-        gpa.free(self.chars);
-    }
-};
-
-inline fn fileSearchZip(
-    io: std.Io,
-    gpa: std.mem.Allocator,
-    prefix: []const u8,
-    comptime exts_zip: []const []const u8,
-) !void {
-    const N = exts_zip.len;
-
-    var arana: std.heap.ArenaAllocator = .init(gpa);
-    defer arana.deinit();
-
-    const aa = arana.allocator();
-    var zipLists: [N]PathList = undefined;
-
-    for (0..N) |i| {
-        zipLists[i] = findExt(io, aa, prefix, exts_zip[0]) catch {
-            return error.NoReults;
-        };
-    }
-
-    const fresh_val: [N]?u32 = .{null} ** N;
-    var found_map: std.StringHashMap([N]?u32) = .init(aa);
-    try found_map.ensureTotalCapacity(32);
-
-    for (zipLists, 0..) |list, ext_idx| {
-        for (list.items, 0..) |element, list_idx| {
-            const almost_key = std.mem.trimStart(u8, element, prefix);
-            const key = std.mem.trimEnd(u8, almost_key, exts_zip[ext_idx]);
-
-            if (found_map.getPtr(key) == null) try found_map.put(key, fresh_val);
-
-            if (found_map.getPtr(key)) |val| val[ext_idx] = @as(u32, @intCast(list_idx));
-        }
-    }
-
-    var names2sort: std.ArrayList([]const u8) = try .initCapacity(aa, defPathListSize);
-    {
-        defer std.mem.sort([]const u8, names2sort.items, {}, stringCompGrow);
-        var it = found_map.iterator();
-        while (it.next()) |e| {
-            var valid = true;
-            for (e.value_ptr) |val| valid = (valid and val != null);
-            if (valid) {
-                try names2sort.append(aa, e.key_ptr.*);
-            }
-        }
-    }
-
-    const results_num = names2sort.items.len;
-    var char_size: usize = 0;
-    for (names2sort.items) |name| {
-        for (found_map.get(name).?, 0..) |inices, group_idx| {
-            const path_len = zipLists[group_idx].items[inices.?].len;
-            char_size += path_len;
-        }
-        std.debug.print("+++ pared name {s}\n", .{name});
-    }
-
-    var packs = try gpa.alloc([]const []const u8, results_num);
-    var names = try gpa.alloc([]const u8, results_num * N);
-    var chars = try gpa.alloc(u8, char_size);
-    defer {
-        gpa.free(packs);
-        gpa.free(names);
-        gpa.free(chars);
-    }
-    names[0] = "essa";
-    packs[0] = &.{"essa"};
-
-    var char_offset: usize = 0;
-    for (names2sort.items) |name| {
-        const ok = found_map.get(name).?;
-        for (0.., ok) |ext_idx, str_idx| {
-            const path = zipLists[ext_idx].items[str_idx.?];
-            const sub = chars[char_offset..][0..path.len];
-            @memcpy(sub, path);
-            char_offset += path.len;
-        }
-    }
-}
-
 pub fn serdesLoad(io: std.Io, gpa: std.mem.Allocator) !meagen.Image {
     const cwd = std.Io.Dir.cwd();
     const prefix = "./fs/serdes";
+    const fake_prefix = "./fs/fake_serdes";
 
-    fileSearchZip(io, gpa, prefix, &.{ ".serdes", "serdes.mono" }) catch |err| {
-        std.debug.print("!!! error searching files {s}\n", .{@errorName(err)});
+    std.debug.print("++++++ are we cooking?\n", .{});
+    files.zipSearch(io, gpa, prefix, &.{ ".serdes", ".serdes.mono" }) catch |err| {
+        // we could spawn fake serdes hear as a test, or just manually creat
+        //      serdes_a.serdes & serdes_b.serdes
+        // into prefix dir
+        files.zipSearch(io, gpa, fake_prefix, &.{ ".serdes", ".serdes.mono" }) catch |eerr| {
+            std.debug.print("!!! fake serdes error | {s}\n", .{@errorName(eerr)});
+        };
+        std.debug.print("!!! serdes error | {s}\n", .{@errorName(err)});
     };
 
     const foundname = findSedes(io, prefix) catch |err| {
